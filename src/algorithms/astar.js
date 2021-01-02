f7534af3cbadf2d26a0f48b48f3c95f661ce2e1e
John Furlong <johnfurlong24@gmail.com>
2021-01-02 12:45:16 -0700

    Started implementing astar

--- src/algorithms/astar.js
+++ src/algorithms/astar.js
@@ -5,11 +5,29 @@
 //      - Goal Node
 //  - Output:
 //      - Guarenteed shortest possible path
-
-export function astar(grid, start, goal){
+function astar(grid, start, goal){
     if(!start || !goal || start == goal)
         return false;
 
-    
+    grid[start].distance = 0;
+    grid[start].totalDistance = 0;
+    grid[start].direction = "N";
+    let frontier = Object.keys(grid);
+    while(frontier.length){
+        let node = closestNode(grid, frontier);
+    }
+}
 
+function closestNode(grid, frontier){
+    let node, index;
+    for(let i = 0; i < frontier.length; i++){
+        if(!node || node.totalDistance > grid[frontier[i]].totalDistance){
+            node = grid[frontier[i]];
+            index = i;
+        }
+        else if(node.totalDistance === grid[frontier[i]].totalDistance && node.heuristicDist > grid[frontier[i]].heuristicDist){
+            node = grid[frontier[i]];
+            index = i; 
+        }
+    }
 }

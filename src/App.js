014f3932356f49079a5061be4e32275db5b850f5
John Furlong <johnfurlong24@gmail.com>
2021-01-04 12:32:47 -0700

    Restructuring react to be hosted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 import PathfindingVisualizer from './pathfindingVisualizer/pathfindingVisualizer';
 import Navbar from './navbar';
 
 function App() {
-  return ( 
+  return (    
+    <div className="App">
       <PathfindingVisualizer></PathfindingVisualizer>
+    </div> 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <div>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </div>,
-
-  document.getElementById('board')
-);
+ReactDOM.render(<App />, document.getElementById('board'));

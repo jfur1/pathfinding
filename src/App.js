b4ffdeae12ad9ed70f5c0281b633d6e7baf8ce69
John Furlong <johnfurlong24@gmail.com>
2021-01-04 13:58:24 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import PathfindingVisualizer from './pathfindingVisualizer/pathfindingVisualizer';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 
 function App() {
   return (    
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar/>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );

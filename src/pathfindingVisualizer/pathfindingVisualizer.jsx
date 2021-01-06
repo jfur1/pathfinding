c877ef58b2d3df801e547d56e6d1bcf4adbdf066
John Furlong <johnfurlong24@gmail.com>
2021-01-06 15:40:34 -0700

    clear vs init grid

--- src/pathfindingVisualizer/pathfindingVisualizer.jsx
+++ src/pathfindingVisualizer/pathfindingVisualizer.jsx
@@ -140,25 +140,27 @@ export default class PathfindingVisualizer extends Component {
       }
   }
   // Same as init grid, exept walls & start/goal nodes are kept
-  clearGrid() {
+  clearGrid(reset) {
     if(this.algoFinished){
+
       for(let row = 0; row < 20; row++) {
         for (let col = 0; col < 50; col++) {
-
+          let node = this.state.grid[row][col];
           if(row === START_NODE_ROW && col === START_NODE_COL){
             document.getElementById(`node-${row}-${col}`).className = 'node node-start';
           }
           else if(row === FINISH_NODE_ROW && col === FINISH_NODE_COL){
             document.getElementById(`node-${row}-${col}`).className = 'node node-finish';
           }
-          else if(document.getElementById(`node-${row}-${col}`).className === "node node-visited"){
+          else if(reset === true){
             document.getElementById(`node-${row}-${col}`).className = 'node';
-          }
-          else if(document.getElementById(`node-${row}-${col}`).className === "node node-shortest-path"){
-            document.getElementById(`node-${row}-${col}`).className = 'node';
-          }
+            node.isWall = false;
+          }else{
+            if(document.getElementById(`node-${row}-${col}`).className !== "node node-wall"){
+              document.getElementById(`node-${row}-${col}`).className = 'node';
+            }
+          
           // Reset data associated with node for the new search
-          let node = document.getElementById(`node-${row}-${col}`);
           node.previousNode = null;
           node.g = Infinity;
           node.h = 0;
@@ -166,6 +168,7 @@ export default class PathfindingVisualizer extends Component {
           node.isVisited = false;
           node.status = 'node';
           this.state.grid[row][col] = node;
+          }
         }
       }
     }
@@ -194,7 +197,7 @@ export default class PathfindingVisualizer extends Component {
       }
       grid.push(tmpRow);
     }
-    return [grid, nodes];
+    //return [grid, nodes];
   };
 
   render() {

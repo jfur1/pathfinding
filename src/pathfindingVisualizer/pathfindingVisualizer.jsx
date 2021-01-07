51b649a92995f823f3c16c94a979348dd08e638c
John Furlong <johnfurlong24@gmail.com>
2021-01-06 17:51:27 -0700

    moving frunction

--- src/pathfindingVisualizer/pathfindingVisualizer.jsx
+++ src/pathfindingVisualizer/pathfindingVisualizer.jsx
@@ -95,15 +95,16 @@ export default class PathfindingVisualizer extends Component {
       else{
         console.log("Selected Algorithm:", algo);
         // Clear any visited nodes from the grid
-        this.clearGrid();
+        
         document.getElementById("startButton").disabled = true;
         document.getElementById("clearGridButton").disabled = true;
       
         this.algoFinished = false;
 
-        var {grid, nodes} = this.state;
-        var start = grid[START_NODE_ROW][START_NODE_COL];
-        var goal = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+        const {grid, nodes} = this.state;
+        const start = grid[START_NODE_ROW][START_NODE_COL];
+        const goal = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+        this.clearGrid();
         var visited = [];
         var path = [];
         // Select algo based on start button text

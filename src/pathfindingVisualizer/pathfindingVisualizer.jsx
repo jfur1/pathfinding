b8e6b23240250653bdbb2b8990b551678f474a4d
John Furlong <johnfurlong24@gmail.com>
2021-01-06 18:58:08 -0700

    help

--- src/pathfindingVisualizer/pathfindingVisualizer.jsx
+++ src/pathfindingVisualizer/pathfindingVisualizer.jsx
@@ -152,12 +152,14 @@ export default class PathfindingVisualizer extends Component {
           }
           else if(document.getElementById(`node-${row}-${col}`).className === "node node-visited"){
             document.getElementById(`node-${row}-${col}`).className = 'node';
+            this.state.grid[row][col].previousNode = null;
+            this.state.grid[row][col].isVisited = false;
           }
           else if(document.getElementById(`node-${row}-${col}`).className === "node node-shortest-path"){
             document.getElementById(`node-${row}-${col}`).className = 'node';
+            this.state.grid[row][col].previousNode = null;
+            this.state.grid[row][col].isVisited = false;
           }
-          this.state.grid[row][col].previousNode = null;
-          this.state.grid[row][col].isVisited = false;
         }
       }
     }

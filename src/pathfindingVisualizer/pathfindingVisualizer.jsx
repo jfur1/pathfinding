526eb87a23e83dbbac96db9863c2fbebcf7760ea
John Furlong <johnfurlong24@gmail.com>
2021-01-06 17:45:06 -0700

    reset nodes to be not visited

--- src/pathfindingVisualizer/pathfindingVisualizer.jsx
+++ src/pathfindingVisualizer/pathfindingVisualizer.jsx
@@ -145,6 +145,7 @@ export default class PathfindingVisualizer extends Component {
       for(let row = 0; row < 20; row++) {
         for (let col = 0; col < 50; col++) {
           this.state.grid[row][col].previousNode = null;
+          this.state.grid[row][col].isVisited = false;
           if(row === START_NODE_ROW && col === START_NODE_COL){
             document.getElementById(`node-${row}-${col}`).className = 'node node-start';
           }
